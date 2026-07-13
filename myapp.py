7232b54fe6a2af6729292664dd1054bc2190158c
Varad Pise <pisevarad7@gmail.com>
2026-07-13 13:32:15 +0530

    python-docker-demo code

---
+++ myapp.py
@@ -0,0 +1,5 @@
+print('Program to sum two numbers')
+num1 = int(input('Enter the first no '))
+num2 = int(input('Enter the second no '))
+result = num1 + num2
+print(f'Sum of two numbers are {result}')
